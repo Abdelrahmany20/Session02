0a342188decbbdfcebd803ce9236740e5e09ff0e
Abdelrahmany20 <abdelrahman.yasser.m17@gmail.com>
2024-12-12 15:51:57 +0200

    Add project files.

---
+++ 1/Program.cs
@@ -0,0 +1,38 @@
+﻿namespace _1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Value Type Casting
+            #region Implicit Casting - Safe Casting
+            //int X = 4; // 4 Bytes
+            //long Y =/*(long)*/X;
+            #endregion
+
+            #region Explict Casting - UnSafe Casting
+            //long X = 101010101010; // 8 Bytes
+            //int Y = (int)X;
+            //Console.WriteLine(Y);
+
+
+            //int Y;
+            //checked
+            //{
+            //Y = (int)X;
+            //}
+
+
+
+            //int X = 88;
+            //double Y = /*(double)*/X;
+
+            //double X = 88.8;
+            //int Y = (int)X;
+            #endregion
+
+
+            #endregion
+        }
+    }
+}
